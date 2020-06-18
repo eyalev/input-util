fd1724b622069c76007e246181a828b14fa73836
Eyal Levin <eyalev@gmail.com>
2020-06-19 01:02:33 +0300

    Update

---
+++ tests/test_input_util.py
@@ -0,0 +1,37 @@
+
+from input_util import I
+
+
+def test_to_json_string():
+    assert I({}).to_json_string() == "{}"
+
+
+def test_is_url():
+    assert I("example.com").is_url
+    assert I("example.com j").is_url is False
+
+
+def test_line_count_with_extra_new_line():
+
+    text = """\
+one
+two
+three
+"""
+
+    line_count = I(text).line_count_with_text
+
+    assert line_count == 3
+
+def test_line_count():
+
+    text = """\
+one
+two
+three\
+"""
+
+    line_count = I(text).line_count_with_text
+
+    assert line_count == 3
+
